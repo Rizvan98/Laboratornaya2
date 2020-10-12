90bdc4eec2ae7e2324129f012b6ca9511d81da19
RIzvan98 <rizvan.98@mail.ru>
2020-10-12 12:18:57 +0600

    code

--- Laboratornaya2/Laboratornaya2/Program.cs
+++ Laboratornaya2/Laboratornaya2/Program.cs
@@ -6,7 +6,18 @@ namespace Laboratornaya2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Rizvan!");
+            string text = "1 - пример с sin \n" +
+                "2 - пример без sin \n";
+
+            Console.Write(text + "Введите одно из двух значений > ");
+            int result = Convert.ToInt32(Console.ReadLine());
+
+            switch(result)
+            {
+                case 1: { Console.WriteLine("j = - sin^3(y)/1*3 + sin^5(x^2)/3*5 - sin^7(y^3)/5*7+sin^9(x^4)/7*9"); break; }
+                case 2: { Console.WriteLine("j = - y/1*3 + x^2/3*5 - y^3/5*7 + x^4/7*9"); break; }
+                default: { Console.WriteLine("Неверное значение"); break; }
+            }
         }
     }
 }
